ffe4dee1983c92dbdef7a82c846c23cc0b411974
Rafael Affonso <hrafaelaf@gmail.com>
2021-03-09 14:53:21 -0300

    feat: add routes and domain for user

--- controllers/user.go
+++ controllers/user.go
@@ -1,8 +1,41 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"users-api/domain"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
+	var user domain.User
+	data, err := ioutil.ReadAll(c.Request.Body)
+
+	if err != nil {
+		// todo: handle error
+		return
+	}
+
+	if err := json.Unmarshal(data, &user); err != nil {
+		// todo: handle error
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "ok",
+		"user":    user,
+	})
+}
+
+func GetAll(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+		"status":  true,
+	})
+}
+
+func FindUser(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
 		"status":  true,
